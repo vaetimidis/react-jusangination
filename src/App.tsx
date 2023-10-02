01cf26233b6070a7de5aa36b35f45e52ca437f25
vaetimidis <othanisushi@gmail.com>
2023-10-02 10:47:22 +0400

    remove unnecessary layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 // import { Game } from './components/Game/Game';
-import { Layout } from './components/layouts/Layout';
 import './style.scss';
 
 function App() {
     return (
-        <Layout>
-            <div className="wrapper">
-                cringe below:
-                <ul className="wrapper__list">
-                    <li>
-                        <a href="/game">ttt</a>
-                    </li>
-                    <li>
-                        <a href="/tasks">list</a>
-                    </li>
-                </ul>
-            </div>
-        </Layout>
+        <div className="wrapper">
+            cringe below:
+            <ul className="wrapper__list">
+                <li>
+                    <a href="/game">ttt</a>
+                </li>
+                <li>
+                    <a href="/tasks">list</a>
+                </li>
+            </ul>
+        </div>
     );
 }
 
