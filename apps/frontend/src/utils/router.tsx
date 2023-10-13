dc9b9fb6aedf7e8e839ee077380c07c96f68aaf0
vaetimidis <othanisushi@gmail.com>
2023-10-13 13:01:03 +0400

    pages && utils

---
+++ apps/frontend/src/utils/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { TodoList } from '#/components/TodoList/TodoList';
+import ErrorPage from '#/routes/404';
+
+enum AppRoutes {
+  HOME = '/',
+  TASKS = '/tasks'
+}
+
+const router = createBrowserRouter([
+  {
+    path: AppRoutes.HOME,
+    element: <div>ggd</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: AppRoutes.TASKS,
+    element: <TodoList />,
+    errorElement: <ErrorPage />
+  }
+]);
+
+export default router;
